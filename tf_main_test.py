0e52f90fd2a7f3018e8d0e1fafc707e82e5b44d5
Tmn07 <519043202@qq.com>
2017-12-30 14:10:16 +0800

    combine style loss

--- tf_main_test.py
+++ tf_main_test.py
@@ -4,44 +4,93 @@ import tensorflow as tf
 import numpy as np
 
 import skimage.io
+import skimage.transform
+
 from scipy.misc import toimage
 from scipy.misc import imsave
 
 from functools import reduce
 
+import custom_vgg19 as vgg19
+
+
 
 kron = tf.contrib.kfac.utils.kronecker_product
 
-# def binary(data):
-#     I = tf.zeros(data.shape)
-#     h, w = data.shape
-#     for i in range(h):
-#         for j in range(w):
-#             if data[i][j] > 0.4:
-#                 I[i][j] = 1
-#     return I
 
-# def twopoint_correlation(data):
-#     s = 0
-#     h = data.shape[0]
-#     w = data.shape[1]
-#     for i1 in range(h):
-#         for j1 in range(w):
-#             for i2 in range(h):
-#                 for j2 in range(w):
-#                     s += tf.multiply(data[i1][j1], data[i2][j2])
-#     # s = tf.expand_dims(s, 0)
-#     return s
+
+def load_image(path):
+    # Load image [height, width, depth]
+    img = skimage.io.imread(path) / 255.0
+    assert (0 <= img).all() and (img <= 1.0).all()
+
+    # Crop image from center
+    short_edge = min(img.shape[:2])
+    yy = int((img.shape[0] - short_edge) / 2)
+    xx = int((img.shape[1] - short_edge) / 2)
+    shape = list(img.shape)
+
+    crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
+    resized_img = skimage.transform.resize(crop_img, (shape[0], shape[1]))
+    
+    shape = [1] + shape
+    resized_img = resized_img.reshape(shape).astype(np.float32)
+
+    return resized_img, shape
+
+def render_img(session, x, out_path):
+    shape = x.get_shape().as_list()
+    img = np.clip(session.run(x), 0, 1)
+
+    toimage(np.reshape(img, shape[1:])).save(out_path)
+
+
+
+def convert_to_gram(filter_maps):
+    dimension = filter_maps.get_shape().as_list()
+    reshaped_maps = tf.reshape(filter_maps, [dimension[1] * dimension[2], dimension[3]])
+    Ml = dimension[1] * dimension[2]
+    return tf.matmul(reshaped_maps, reshaped_maps, transpose_a=True) / Ml
+
+def get_l2_norm_loss(diffs):
+    shape = diffs.get_shape().as_list()
+    # size = reduce(lambda x, y: x * y, shape) ** 2
+    size = shape[0]*shape[1]
+    sum_of_squared_diffs = tf.reduce_sum(tf.square(diffs))
+    return sum_of_squared_diffs / size
+
+def get_texture_loss(x, s):
+    with tf.name_scope('get_style_loss'):
+        texture_layer_losses = [get_texture_loss_for_layer(x, s, l) for l in TEXTURE_LAYERS]
+        texture_weights = tf.constant([1. / len(texture_layer_losses)] * len(texture_layer_losses), tf.float32)
+        weighted_layer_losses = tf.multiply(texture_weights, tf.convert_to_tensor(texture_layer_losses))
+        return tf.reduce_sum(weighted_layer_losses)
+
+def get_texture_loss_for_layer(x, s, l):
+    with tf.name_scope('get_style_loss_for_layer'):
+        x_layer_maps = getattr(x, l)
+        t_layer_maps = getattr(s, l)
+        x_layer_gram = convert_to_gram(x_layer_maps)
+        t_layer_gram = convert_to_gram(t_layer_maps)
+
+        # shape = x_layer_maps.get_shape().as_list()
+        # size = reduce(lambda a, b: a * b, shape) ** 2
+        gram_loss = get_l2_norm_loss(x_layer_gram - t_layer_gram)
+        return gram_loss
+
+
 
 def twopoint_correlation(data):
     shape = data.get_shape().as_list()
     # print (data.shape)
     size = shape[0] * shape[1] * shape[0] * shape[1]
-    res = kron(data[:,:,0], data[:,:,0])
+    res = kron(data, data)
+    # res = kron(data[:,:,0], data[:,:,0])
     s = tf.reduce_sum(res)
+    return s
     # print(s)
     # s.reshape([1])
-    return tf.reshape(s,[1])
+    # return tf.reshape(s,[1])
 
 def minmax_scaling(data):
     ma = tf.reduce_max(data)
@@ -53,28 +102,21 @@ def twopoint_correlation_layer(data, kernel_size=(3, 3)):
 
     data = minmax_scaling(data)
 
-    # print(data.shape)
     h = data.shape[0]
     w = data.shape[1]
-    # s_m = []
+    th = h - kernel_size[0] + 1
+    tw = w - kernel_size[0] + 1
+    s_m = []
     for i in range(h - kernel_size[0] + 1):
-        s_row = []
         for j in range(w - kernel_size[1] + 1):
             s = twopoint_correlation(data[i:i + kernel_size[0], j:j + kernel_size[1]])
-            print(s)
-            s_row = tf.concat([s_row, s], 0)
+            s_m.append(s)
 
-        if i == 0:
-            s_m = tf.expand_dims(s_row, 0)
-        else:
-            s_row = tf.expand_dims(s_row, 0)
-            s_m = tf.concat([s_m, s_row], 0)
-            # print(s_row.eval())
-            # print(s_m.eval())
+    s_m = tf.reshape(tf.stack(s_m), [th, tw])
     return s_m
 
 
-def get_l2_norm_loss(diffs):
+def l2_loss(diffs):
     shape = diffs.get_shape().as_list()
     # size = reduce(lambda x, y: x * y, shape) ** 2
     size = shape[0]*shape[1]
@@ -83,20 +125,19 @@ def get_l2_norm_loss(diffs):
 
 
 if __name__ == '__main__':
-    # from keras.datasets import mnist
-    #
-    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    # x_train = x_train.astype('float32')
-    # img_rows, img_cols = 28, 28
-    # x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    # x_train /= 255
 
-    EPOCHS = 300
+    EPOCHS = 30
+    TEXTURE_LAYERS = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1']
+    height = 10
+    width = 10
+    image_shape = (height, width)
+    Alpha = 1
+    Beta = 1
 
-    height = 30
-    width = 30
-    image_shape = (height, width, 1)
+    # , 1)
 
+
+    
     noise_init = tf.truncated_normal(image_shape, mean=.5, stddev=.1)
 
     # img = skimage.io.imread("res/result-0.png", as_grey=True) / 255.0
@@ -108,14 +149,34 @@ if __name__ == '__main__':
     img = skimage.io.imread("tex1.png", as_grey=True) / 255.0
     # img = np.array([[0.4,0.6,0.1,0.2],[0.1,0.5,0.5,0],[1,1,0.4,0.3],[0.2,0.2,0.5,0.8]])
     timg = tf.convert_to_tensor(img, dtype=tf.float32)
-    timg = tf.expand_dims(timg, 2)
+    # timg = tf.expand_dims(timg, 2)
     # print(timg.shape)
-
 
     gen_sm = twopoint_correlation_layer(noise)
     sm = twopoint_correlation_layer(timg[:height, :width])
 
-    total_loss = get_l2_norm_loss(gen_sm - sm)
+    tpc_loss = l2_loss(gen_sm - sm)
+
+    # compute style loss
+
+    texture_img, texture_shape = load_image("tex3.png")
+    # h,w,c
+    texture_model = vgg19.Vgg19()
+    texture_model.build(texture_img, texture_shape[1:])
+    
+    
+    
+    rgbnoise = tf.expand_dims(noise, 2)
+    rgbnoise = tf.image.grayscale_to_rgb(rgbnoise)
+    rgbnoise = tf.expand_dims(rgbnoise, 0)
+    # print(rgbnoise.shape)
+    x_model = vgg19.Vgg19()
+    x_model.build(rgbnoise, rgbnoise.shape.as_list()[1:])
+
+    style_loss = get_texture_loss(x_model, texture_model)
+
+
+    total_loss = Alpha * tpc_loss + Beta * style_loss
 
     optimizer = tf.train.AdamOptimizer(0.02).minimize(total_loss, var_list=noise)
 
@@ -126,8 +187,8 @@ if __name__ == '__main__':
         # print(sess.run(noise))
 
         for i in range(1, EPOCHS):
-            _, loss, xxx = sess.run([optimizer, total_loss, noise])
+            _, ls1, ls2, xxx = sess.run([optimizer, tpc_loss, style_loss, noise])
             # print(xxx.reshape(height, width))
-            print("Epoch %d | Loss: %.03f\n" % (i, loss))
-            # np.reshape()
-            imsave('res/result-%d.png' % i, xxx.reshape(height, width))
+            print("Epoch %d | tpc_loss: %.03f\n | style_loss: %.03f\n" % (i, ls1, ls2))
+            if i % 10 == 0:
+                imsave('res/result-%d.png' % i, xxx.reshape(height, width))
